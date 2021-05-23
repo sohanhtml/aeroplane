aaccabc294f9ae5ea046e90403bf598d7890ae87
Sohan <sohanhtml@gmail.com>
2021-05-23 12:45:40 +0530

    Sohan changes

--- sketch.js
+++ sketch.js
@@ -40,6 +40,7 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+    text("angry birds", 50 , 50);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
